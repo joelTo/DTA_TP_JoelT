97d120ada472f0ba4882e48456bdad90ae63ed66
joel T <joel.torib@gmail.com>
2016-11-22 14:13:46 +0100

    1er initialisation du fichier

---
+++ TP 1 JAVA/BonjourPersonnalise.java
@@ -0,0 +1,16 @@
+/**
+Creation de la class Bonjour Personnalise
+args (Cest que l'on veut ') 
+*/
+public class BonjourPersonnalise
+{	
+	// Creation de l'argument  
+	public static void main (String[] args)
+	{
+		System.out.println("Bonjour "+ args[0]);
+		/*
+		Ici on ecrit ce que l'on veut
+		Et oui!!!!
+		*/
+	}
+}
